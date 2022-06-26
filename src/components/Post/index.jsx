cbc3f07004df45e9061ed3bc2d09503281ad4dad
Виктор Гончаров <t-bass@mail.ru>
2022-06-26 23:37:24 +0300

    добавляем часть функционала

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import clsx from 'clsx';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Clear';
@@ -34,12 +36,12 @@ export const Post = ({
     <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
       {isEditable && (
         <div className={styles.editButtons}>
-          <a href={`/posts/${_id}/edit`}>
-            <IconButton color="primary">
+          <Link to={`/posts/${_id}/edit`}>
+            <IconButton color='primary'>
               <EditIcon />
             </IconButton>
-          </a>
-          <IconButton onClick={onClickRemove} color="secondary">
+          </Link>
+          <IconButton onClick={onClickRemove} color='secondary'>
             <DeleteIcon />
           </IconButton>
         </div>
@@ -55,12 +57,12 @@ export const Post = ({
         <UserInfo {...user} additionalText={createdAt} />
         <div className={styles.indention}>
           <h2 className={clsx(styles.title, { [styles.titleFull]: isFullPost })}>
-            {isFullPost ? title : <a href={`/posts/${_id}`}>{title}</a>}
+            {isFullPost ? title : <Link to={`/posts/${_id}`}>{title}</Link>}
           </h2>
           <ul className={styles.tags}>
             {tags.map((name) => (
               <li key={name}>
-                <a href={`/tag/${name}`}>#{name}</a>
+                <Link to={`/tag/${name}`}>#{name}</Link>
               </li>
             ))}
           </ul>
